b74ee13d8e136c73073c4d52bff08f33f1804a3d
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-11-14 20:28:49 -0800

    10-minute update

--- 210-lab-32.cpp
+++ 210-lab-32.cpp
@@ -6,12 +6,25 @@
 
 using namespace std;
 
+const int INITIAL_SIZE = 2;
+
+// printQueue() calls the
+void printQueue(deque<Car> &);
+
 int main() {
-	/*Use an std::deque. This means our program structure will be a deque of Cars.
-	 *Use a constant and initialize it to 2 to determine the initial size of the deque when the simulation starts
-	 *(representing how many cars are already in line to pay the toll when the simulation starts).
-	 *Use the Car class to create Car objects, which are placed in the std::deque representing the toll booth lane.
-	 *Do not alter the Car class.
-	 *Run the simulation until the queue is empty.
+	/*Use the Car class to create Car objects, which are placed in the std::deque representing the toll booth lane.
 	 *After each time period, display the queue using the .print() method in the Car class.*/
+
+	deque<Car> cars(INITIAL_SIZE);
+
+	cout << "Initial queue:" << endl;
+	for (auto car : cars) {
+		car.print();
+	}
+
+	while (!cars.empty()) {
+		// Each loop simulates the time period
+		// 55% chance: Car at the head pays its toll and leaves the toll booth
+		// 45% chance: Car joins the line for the toll booth
+	}
 }
