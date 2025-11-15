dfb612624f2ba3e7f09f5d646f9e57495fe15b8e
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-11-14 20:18:10 -0800

    Initial commit

---
+++ 210-lab-32.cpp
@@ -0,0 +1,17 @@
+// COMSC-210 | Lab 32 | Dainiz Almazan
+// IDE used: CLion
+
+#include "Car.h"
+#include <deque>
+
+using namespace std;
+
+int main() {
+	/*Use an std::deque. This means our program structure will be a deque of Cars.
+	 *Use a constant and initialize it to 2 to determine the initial size of the deque when the simulation starts
+	 *(representing how many cars are already in line to pay the toll when the simulation starts).
+	 *Use the Car class to create Car objects, which are placed in the std::deque representing the toll booth lane.
+	 *Do not alter the Car class.
+	 *Run the simulation until the queue is empty.
+	 *After each time period, display the queue using the .print() method in the Car class.*/
+}
